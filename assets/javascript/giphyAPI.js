1b0f157e743460e652e2019991a79043eb2edcf6
Tolu Idowu <itolu94@gmail.com>
2017-03-28 16:04:34 -0400

    Functionality present...  Need to work on styling

---
+++ assets/javascript/giphyAPI.js
@@ -0,0 +1,61 @@
+var gameShows = ["Family Fued", "Price is Right", "Deal or No Deal", "wheel of Fortune", "Jepordy", "Newly Weds"];
+
+
+
+
+
+$('#gsButtonGenorator').on('click', function() {
+    event.preventDefault();
+    var newInput = $('#gsSearchBar').val().trim();
+    gameShows.push(newInput);
+    // Emptys div buttons are in
+    $('#buttonDiv').empty();
+
+    // repopulate the buttonDiv with new arrary
+    for (var i = 0; i < gameShows.length; i++) {
+        var newButton = $('<button>');
+        newButton.html(gameShows[i]).attr('class', 'gs');
+        $('#buttonDiv').append(newButton);
+    }
+});
+
+
+$('#buttonDiv').on('click', '.gs', function() {
+    // get the users input and if spaces are in 
+    var queryId = ($(this).html()).replace(/ /g, '+');
+    queryURL = "http://api.giphy.com/v1/gifs/search?q= " + queryId + "&limit=10&api_key=dc6zaTOxFJmzC"
+    console.log(queryURL);
+    $.ajax({
+        method: "GET",
+        url: queryURL
+    }).done(function(response) {
+        // console.log(response);
+        // console.log(response.data[0].images.original.url);
+        // console.log(response.data[0].images.original_still.url);
+        $('#gifsDiv').empty();
+        for (var i = 0; i < response.data.length; i++) {
+            var gifImg = $('<img>');
+            var gifImgDiv =  $('<div>');
+            gifImgDiv.attr('class', 'gifsDiv text-center');
+            gifImg.attr({ src: response.data[i].images.original_still.url, state: 'still', 'data-actice': response.data[i].images.original.url, 'data-still': response.data[i].images.original_still.url,})
+            gifImgDiv.append('<p> Rating:  ' + response.data[i].rating +'</p>' , gifImg);
+            $('#gifsDiv').append(gifImgDiv);
+        }
+    })
+
+
+
+
+
+});
+
+
+$('#gifsDiv').on('click', 'img', function() {
+    console.log("Current state is " + $(this).attr('state'));
+    if ($(this).attr('state') === 'still') {
+    	$(this).attr({'src': $(this).attr('data-actice'), state: 'active'});
+    } else if ($(this).attr('state') === 'active'){
+    	$(this).attr({'src': $(this).attr('data-still'), state: 'still'});
+
+    }
+});
