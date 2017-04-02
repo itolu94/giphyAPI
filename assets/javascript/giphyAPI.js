2567d0d130b4fb8ff408b3938f1edd0ef464fd77
Tolu Idowu <itolu94@gmail.com>
2017-04-02 15:57:42 -0400

    Final

--- assets/javascript/giphyAPI.js
+++ assets/javascript/giphyAPI.js
@@ -1,10 +1,8 @@
+// Buttons for shows
 var gameShows = ["Family Fued", "Price is Right", "Deal or No Deal", "wheel of Fortune", "Jepordy", "Newly Weds"];
 
-
-
-
-
-$('#gsButtonGenorator').on('click', function() {
+// Gets users text and generates a button
+$('#gsButtonGenerator').on('click', function() {
     event.preventDefault();
     var newInput = $('#gsSearchBar').val().trim();
     gameShows.push(newInput);
@@ -21,40 +19,36 @@ $('#gsButtonGenorator').on('click', function() {
 
 
 $('#buttonDiv').on('click', '.gs', function() {
-    // get the users input and if spaces are in 
+    // replaces spaces with +
     var queryId = ($(this).html()).replace(/ /g, '+');
     queryURL = "http://api.giphy.com/v1/gifs/search?q= " + queryId + "&limit=9&api_key=dc6zaTOxFJmzC"
     $.ajax({
         method: "GET",
         url: queryURL
     }).done(function(response) {
-        console.log(response);
-        // console.log(response.data[0].images.original.url);
-        // console.log(response.data[0].images.original_still.url);
+        // emptys the div that the divs are held in
         $('#gifsDiv').empty();
+
+        // create div for each image
         for (var i = 0; i < response.data.length; i++) {
             var gifImg = $('<img>');
-            var gifImgDiv =  $('<div>');
+            var gifImgDiv = $('<div>');
             gifImgDiv.attr('class', 'gifsDiv text-center');
-            gifImg.attr({ src: response.data[i].images.fixed_height_still.url, state: 'still', 'data-actice': response.data[i].images.fixed_height.url, 'data-still': response.data[i].images.fixed_height_still.url, 'class': 'gifImage'})
-            gifImgDiv.append('<p> Rating:  ' + response.data[i].rating +'</p>' , gifImg);
+            gifImg.attr({ src: response.data[i].images.fixed_height_still.url, state: 'still', 'data-actice': response.data[i].images.fixed_height.url, 'data-still': response.data[i].images.fixed_height_still.url, 'class': 'gifImage' })
+            gifImgDiv.append('<p> Rating:  ' + response.data[i].rating + '</p>', gifImg);
             $('#gifsDiv').append(gifImgDiv);
         }
     })
-
-
-
-
-
 });
 
 
+// Changes the state of the gif when it was clicked
 $('#gifsDiv').on('click', 'img', function() {
     console.log("Current state is " + $(this).attr('state'));
     if ($(this).attr('state') === 'still') {
-    	$(this).attr({'src': $(this).attr('data-actice'), state: 'active'});
-    } else if ($(this).attr('state') === 'active'){
-    	$(this).attr({'src': $(this).attr('data-still'), state: 'still'});
+        $(this).attr({ 'src': $(this).attr('data-actice'), state: 'active' });
+    } else if ($(this).attr('state') === 'active') {
+        $(this).attr({ 'src': $(this).attr('data-still'), state: 'still' });
 
     }
 });
